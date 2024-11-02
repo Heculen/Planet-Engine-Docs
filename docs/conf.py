2512ea2c21962af7334ace2dd59d83d392e99566
Hechen <158656595+Comdfsfsfas@users.noreply.github.com>
2024-11-02 21:11:54 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
+project = "Planet Engine Docs"
 copyright = "2022, Read the Docs core team"
 author = "Read the Docs core team"
 
